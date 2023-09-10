2b60bcdf78f970921de089b38dbcc49bc83d34dd
Vicente Aguilera <vicente.aguilera2001@alumnos.ubiobio.cl>
2023-09-10 15:29:08 -0300

    SAdd License plate tracker

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 #!/usr/bin/env python3
 import cv2
 import depthai as dai
+from ultralytics import YOLO
 
-pipeline = dai.Pipeline()
+pipeline = dai.Pipeline() # Create pipeline
 
-camRgb = pipeline.create(dai.node.ColorCamera)
-xoutVideo = pipeline.create(dai.node.XLinkOut)
+camRgb = pipeline.create(dai.node.ColorCamera) # Create Color Camera
+xoutVideo = pipeline.create(dai.node.XLinkOut) # Create XLinkOut node
 
-xoutVideo.setStreamName("video")
+xoutVideo.setStreamName("video") # Set stream name
 
-camRgb.setBoardSocket(dai.CameraBoardSocket.CAM_A)
-camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-camRgb.setVideoSize(1920, 1080)
-camRgb.setPreviewSize(300, 300)
-camRgb.setInterleaved(False)
-camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-camRgb.setFps(40)
+camRgb.setBoardSocket(dai.CameraBoardSocket.CAM_A) # Set camera socket
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P) # Set resolution
+camRgb.setVideoSize(1920, 1080) # Set video size
+camRgb.setFps(40) # Set fps
 
-xoutVideo.input.setBlocking(False)
-xoutVideo.input.setQueueSize(1)
+xoutVideo.input.setBlocking(False) # Set blocking
+xoutVideo.input.setQueueSize(1) # Set queue size
 
-camRgb.video.link(xoutVideo.input)
+camRgb.video.link(xoutVideo.input) # Link video
+
+model = YOLO('model/licence_plate.pt')
+results = {}
+last_license_plate = None
+frame_nmr = 0
 
 with dai.Device(pipeline) as device:
-
-    video = device.getOutputQueue(name="video", maxSize=1, blocking=False)
-
+    video = device.getOutputQueue(name="video", maxSize=1, blocking=False) # Get video output queue
     while True:
         videoIn = video.get()
 
-        #Funcionalidad
+        frame = videoIn.getCvFrame()
 
-        cv2.imshow("video", videoIn.getCvFrame())
+        results = model.track(frame, persist=True)
 
+        frame_ = results[0].plot()
+
+        cv2.imshow("video", frame_)
         if cv2.waitKey(1) == ord('q'):
             break
 
